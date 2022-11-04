f72593bae62464b97b1c5aa8816f68b352abe2c8
Yasin <cobanyasin1998@gmail.com>
2022-11-05 00:20:52 +0300

    first init

---
+++ DependencyInjection/Controllers/WeatherForecastController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace DependencyInjection.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        
+        [HttpGet]
+        public String Get()
+        {
+            NumGenerator n = new NumGenerator();
+
+            int number = n.GetRandomNumber();
+
+            return number.ToString();
+        }
+    }
+}
---
+++ DependencyInjection/NumGenerator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DependencyInjection
+{
+    public class NumGenerator
+    {
+        public int GetRandomNumber()
+        {
+           
+
+            return new Random().Next(10);
+        }
+    }
+}
