a59b2de47a5b11eb35b8eafd529e13277d93c872
Yasin <yasincoban1998@outlook.com>
2022-11-06 15:17:42 +0300

    exceptionHandlerMiddlewares added

---
+++ Middlewares/Controllers/WeatherForecastController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Middlewares.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+
+        [HttpGet]
+        public String Get()
+        {
+
+            throw new Exception("TEST HATASI");
+
+
+            return "OK";
+        }
+    }
+}
---
+++ Middlewares/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace Middlewares
+{
+    public class ExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionHandlerMiddleware> _logger;
+        public ExceptionHandlerMiddleware(RequestDelegate next,ILogger<ExceptionHandlerMiddleware> logger)
+        {
+            _logger = logger;
+            _next = next;
+        }
+
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            try
+            {
+                await _next.Invoke(httpContext);
+            }
+            catch (System.Exception e)
+            {
+                //Hata Yönetimi
+                _logger.LogError(e.Message);
+             
+            }
+
+            
+        }
+    }
+}
